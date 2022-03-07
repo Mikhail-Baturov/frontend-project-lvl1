ecd3b02ff25b9a4cba254843c1898e882f85eff3
baturov <baturovm@mail.ru>
2022-03-08 02:35:00 +0300

    fix cli-prime.js

--- src/cli-prime.js
+++ src/cli-prime.js
@@ -1,8 +1,11 @@
 export default function dataForGameBrainPrime() {
   let correctAnswer = '';
   const questionData = Math.floor(Math.random() * 100);
-  let isPrime = true;
   let divisor = 2;
+  let isPrime = true;
+  if (questionData === 0 || questionData === 1) {
+    isPrime = false;
+  }
   while (isPrime && divisor <= questionData / 2) {
     if (questionData % divisor === 0) {
       isPrime = false;
