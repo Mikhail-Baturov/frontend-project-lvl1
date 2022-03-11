4e1f780a47848dddda3c2bb8972369cff2bd989a
baturov <baturovm@mail.ru>
2022-03-11 08:50:42 +0300

    fix findGCD

--- src/games/cli-gcd.js
+++ src/games/cli-gcd.js
@@ -1,6 +1,6 @@
 const findGCD = (num1, num2) => {
-  const number1 = num1;
-  const number2 = num2;
+  let number1 = num1;
+  let number2 = num2;
   while (number1 !== 0 && number2 !== 0) {
     if (number1 > number2) {
       number1 %= number2;
